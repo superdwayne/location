50cc2e5b46010d6de53af07ea2f932c8fd21b2e8
Dwayne Paisley-Marshall <superdwayne@gmail.com>
2023-03-06 12:36:12 +0100

    Location

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react"
+
+
+const App = () => {
+
+const [lat, setLat] = useState(null);
+const [lng, setLng] = useState(null);
+const [status, setStatus] = useState(null);
+
+const getLocation = () => {
+  if (!navigator.getLocation) {
+    setStatus("Not supportted"); 
+  } else 
+  setStatus("Locating");
+  navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setStatus(null);
+        setLat(position.coords.latitude);
+        setLng(position.coords.longitude);
+      }, 
+      () => {
+        setStatus("unable to retrieve youe location");
+      }
+      );
+      }
+    
+
+
+  return (
+    <div className="App">
+      <button onClick={getLocation}>Get location</button>
+      <h1>Coor</h1>
+        <p>Status</p>
+        {lat && <p>latitude: {lat}</p>}
+        {lng && <p>longitude: {lng}</p>}
+    </div>
+  );
+}
+
+export default App;
